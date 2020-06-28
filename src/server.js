fbec9b3df4e72820c79fbc3e384184ce285a0142
Prateek Gupta <prateekg.9501@gmail.com>
2020-06-29 00:20:26 +0530

    installed nodemon

--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const PORT = process.env.PORT || 8000;
 
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello from us');
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`);
