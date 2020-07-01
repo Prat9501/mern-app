940a9ef2b2fb5dbe8f3bf762dd1adcbadf055ea6
Prateek Gupta <prateekg.9501@gmail.com>
2020-07-01 23:45:32 +0530

    splitted dashboard controller

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const multer = require('multer');
 
 const UserController = require('./controllers/UserController');
 const EventController = require('./controllers/EventController');
+const DashboardController = require('./controllers/DashboardController');
 const uploadConfig = require('./config/upload');
 
 const routes = express.Router();
@@ -14,15 +15,17 @@ routes.get('/status', (req, res) => {
     res.send("Status up and running");
 })
 
+//Dashboard
+routes.get('/dashboard', DashboardController.getAllEvents);
+routes.get('/dashboard/:sport', DashboardController.getAllEvents);
+routes.get('/event/:eventId', DashboardController.getEventById);
+
 //Users
 routes.post('/user/register', UserController.createUser);
 routes.get('/user/:userId', UserController.getUserById);
 
 //Event
-routes.get('/events', EventController.getAllEvents);
-routes.get('/events/:sport', EventController.getAllEvents);
 routes.post('/event', img_uploader.single('thumbnail'), EventController.createEvent);
-routes.get('/event/:eventId', EventController.getEventById);
 routes.delete('/event/:eventId', EventController.deleteEvent);
 
 module.exports = routes;
