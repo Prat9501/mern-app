d860fae0394399874a2ced6d0c30e377de11d399
Prateek Gupta <prateekg.9501@gmail.com>
2020-07-11 19:52:28 +0530

    added the eventsPage

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 
 export default function Dashboard(){
+    const user_id = localStorage.getItem('user');
+    console.log(user_id);
     return (
         <div>
             Hello, this is Dashboard
--- frontend/src/pages/EventsPage/index.js
+++ frontend/src/pages/EventsPage/index.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from '../../services/api';
+import { Container, Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import cameraIcon from '../../assets/camera.png';
 
 
 export default function EventsPage(){
+    const user_id = localStorage.getItem('user');
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [price, setPrice] = useState('');
+    const [thumbnail, setThumbnail] = useState('');
+    const [date, setDate] = useState('');
+
     return (
-        <div>
-            Hello, this is EventsPage
-        </div>
+        <Container>
+            <h2>Create your Event</h2>
+            <Form>
+                <Label>Upload Image:  
+                    <img src={cameraIcon} style={{maxWidth:'25px'}} alt='upload icon img' />
+                </Label>
+                <Input 
+                    id='thumbnail' 
+                    type='file'
+                    onChange={(evt) => setThumbnail(evt.target.files[0])}
+                    />
+            </Form>
+        </Container>
     )
 }   
