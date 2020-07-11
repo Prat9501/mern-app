d67fcec81112bdac45e226d3ff97b3ce61382fda
Prateek Gupta <prateekg.9501@gmail.com>
2020-07-12 00:28:50 +0530

    added css to frontend

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,26 +1,35 @@
 import React, { useState } from 'react';
 import api from '../../services/api';
-import { Container, Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Container, Button, Form, FormGroup, Label, Input, Alert } from 'reactstrap';
 
 
 
 export default function Login({ history }){
-    const [ email, setEmail ] = useState('')
-    const [ password, setPassword ] = useState('')
+    const [ email, setEmail ] = useState('');
+    const [ password, setPassword ] = useState('');
+    const [error, setError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('false');
 
     const handleSubmit = async evt => {
         evt.preventDefault();
-        console.log(email, password);
-
         const response = await api.post('/login', { email, password })
         const userId = response.data._id || false;
 
-        if(userId){
-            localStorage.setItem('user', userId)
-            history.push('/dashboard')
-        } else {
-            const { message } = response.data
-            console.log(message);
+        try {
+            if(userId){
+                localStorage.setItem('user', userId)
+                history.push('/dashboard')
+            } else {
+                const { message } = response.data
+                setError(true);
+                setErrorMessage(message);
+                setTimeout(() => {
+                    setError(false)
+                    setErrorMessage('')
+                }, 2000)
+            }
+        } catch (error) {
+            
         }
     }
     
@@ -28,7 +37,7 @@ export default function Login({ history }){
         <Container>
             <h2>Login Form</h2>
             <Form onSubmit={handleSubmit}>
-                <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                <FormGroup>
                     <Label for="exampleEmail" className="mr-sm-2">Email</Label>
                     <Input 
                         type="email" 
@@ -38,7 +47,7 @@ export default function Login({ history }){
                         onChange={evt=> setEmail(evt.target.value)}
                         />
                 </FormGroup>
-                <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                <FormGroup>
                     <Label for="examplePassword" className="mr-sm-2">Password</Label>
                     <Input 
                         type="password" 
@@ -48,8 +57,11 @@ export default function Login({ history }){
                         onChange={evt=> setPassword(evt.target.value)}
                         />
                 </FormGroup>
-                <Button>Submit</Button>
+                <Button type='submit'>Login</Button>
             </Form>
+            {error ? (
+                <Alert className='event-validation' color='danger'>Missing required fields</Alert>
+            ): ''}
         </Container>
     )
 }
