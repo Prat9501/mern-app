8096c4bf7a402adade7997c45e82ef47057125ae
Prateek Gupta <prateekg.9501@gmail.com>
2020-07-14 13:06:22 +0530

    added events to dashboard

--- backend/src/controllers/EventController.js
+++ backend/src/controllers/EventController.js
@@ -5,7 +5,7 @@ const User = require('../models/User');
 module.exports = {
     async createEvent(req, res) {
         try {
-            const {title, description, price, sport} = req.body;
+            const {title, description, price, sport, date} = req.body;
             const { user_id } = req.headers;
             const { filename } = req.file;
             const user = await User.findById(user_id);
@@ -20,6 +20,7 @@ module.exports = {
                 title,
                 description,
                 sport,
+                date,
                 price: parseFloat(price),
                 user: user_id,
                 thumbnail: filename
--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,12 +1,37 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import moment from 'moment';
+import {Button} from 'reactstrap';
+import api from '../../services/api';
+import './dashboard.css';
 
 export default function Dashboard(){
     const user_id = localStorage.getItem('user');
-    console.log(user_id);
+    const [events, setEvents] = useState([]);
+
+    useEffect(() => {
+        getEvents();
+    }, [])
+
+    const getEvents = async (filter) => {
+        const url = filter ? `/dashboard/${filter}` : '/dashboard'
+        const response = await api.get(url, { headers: {user_id}})
+        setEvents(response.data)
+    }
+    console.log(events);
     return (
-        <div>
-            Hello, this is Dashboard
-        </div>
+        <ul className='events-list'>
+            {
+                events.map(event => (
+                    <li key={event._id}>
+                        <header style={{backgroundImage: `url(${event.thumbnail_url})`}} />
+                        <strong>{event.title}</strong>
+                        <span>Event Date: {moment(event.date).format('l')}</span>
+                        <span>Entry Price: Rs {parseFloat(event.price).toFixed(2)}</span>
+                        <span>Event Description: {event.description}</span>
+                        <Button color='primary'>Subscribe</Button>
+                    </li>
+                ))
+            }
+        </ul>
     )
 }
