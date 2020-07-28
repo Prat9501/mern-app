61fb063ac5c5a059390a31d61a43f01d0225db7c
Prateek Gupta <prateekg.9501@gmail.com>
2020-07-28 17:22:08 +0530

    added the myRegistrations page

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,6 +4,7 @@ import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import Register from './pages/Register';
 import EventsPage from './pages/EventsPage';
+import MyRegistrations from './pages/MyRegistrations';
 import TopNav from './components/TopNav';
 
 
@@ -13,6 +14,7 @@ export default function Routes(){
             <TopNav />
             <Switch>
                 <Route path='/' exact component={Dashboard}/>
+                <Route path='/myregistrations' exact component={MyRegistrations}/>
                 <Route path='/login' exact component={Login} />
                 <Route path='/register' exact component={Register} />
                 <Route path='/events' component={EventsPage} />
